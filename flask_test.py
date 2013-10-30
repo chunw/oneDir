577921ba79771c6794cc7abcf949eb3441969fe1
jhc7ej <goskins13>
2013-10-30 02:55:12 -0400

    Flask interaction with database

---
+++ flask_test.py
@@ -0,0 +1,54 @@
+__author__ = 'justin'
+
+from sqlite3 import dbapi2 as sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
+
+# create our little application :)
+app = Flask(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE='/home/justin/PycharmProjects/oneDir-group14/OneDir_accounts.db',
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_object(__name__)
+
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select username from user_account')
+    entries = cur.fetchall()
+    print entries
+    return render_template('show_database.html', entries=entries)
+
+
+if __name__ == '__main__':
+    app.run()
