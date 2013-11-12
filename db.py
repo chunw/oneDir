64c82a1fe9f828c01033edfa799969ee6403b1a5
cts2bu <lot59ym>
2013-11-11 23:09:19 -0500

    First push of user log-in stuff.

--- db.py
+++ db.py
@@ -11,15 +11,18 @@ def createTable():
     queryCurs.execute('''CREATE TABLE user_account
     (username TEXT PRIMARY KEY, PASSWORD TEXT)''')
 
-def addUserAccount(username, password):
-        queryCurs.execute('''INSERT INTO user_account (username, password)
-        VALUES (?, ?)''', (username, password))
+def addUserAccount(username, password, user_type):
+        queryCurs.execute('''INSERT INTO user_account (username, password, user_type)
+        VALUES (?, ?, 'normal')''', (username, password))
 
 def main():
-    createTable()
+    #createTable()
+    queryCurs.execute("ALTER TABLE user_account ADD COLUMN user_type TEXT")
+    #queryCurs.execute("UPDATE user_account SET user_type = 'normal' WHERE username = 'justinc'")
     #queryCurs.execute('''DELETE  FROM user_account where username = 'justinc' ''')
     #addUserAccount('justinc', 'qwerty')
     #addUserAccount('user1', 'password')
+    #addUserAccount('smithc', 'password', 'normal')
     createDb.commit()
     queryCurs.execute('SELECT * FROM user_account')
 
