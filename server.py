8ec1c2e66345325ead770735d1cdc28348409a5d
Chun Wang <chun@spanishdict.com>
2013-11-22 20:45:57 -0500

    Update server to handle file uploading requests.

--- server.py
+++ server.py
@@ -1,28 +1,47 @@
-from flask import Flask,request
 import os
+from flask import Flask, request
 from flask import send_from_directory
+from werkzeug.utils import secure_filename
 from os.path import expanduser
 
-fileApp = Flask(__name__, static_folder = 'static', static_url_path = '/')
-
 home = expanduser("~")
-UPLOAD_FOLDER = home + '/uploads'  # assume client user has a dir called uploads under home dir
+UPLOAD_FOLDER = home + '/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'py', 'cpp', 'h', 'java', 'doc'])
+
+fileApp = Flask(__name__, static_folder = 'static', static_url_path = '/')
 fileApp.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@fileApp.route('/', methods=['GET', 'POST', 'curl'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+# save files uploaded to this route to UPLOAD_FOLDER
+@fileApp.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        return request.txt
+        file = request.files['filedata']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(fileApp.config['UPLOAD_FOLDER'], filename))
+            # let client know about this update
 
-     #   file = request.files['file']
-      #  if file:
-       #     filename = file.filename
-        #    file.save(os.path.join(fileApp.config['UPLOAD_FOLDER'], filename))
-    #return "Welcome to OneDir-group14"
 
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+    <p><input type=file name=file>
+    <input type=submit value=Upload>
+    </form>
+     '''
+
+# view files uploaded to UPLOAD_FOLDER
 @fileApp.route('/uploads/<filename>')
-def uploaded_file(filename):
+def uploaded_files(filename):
     return send_from_directory(fileApp.config['UPLOAD_FOLDER'], filename)
 
+
 if __name__ == '__main__':
-    fileApp.run(host='0.0.0.0',port=8080)
+    fileApp.run(host='0.0.0.0', port=8080, debug=True)
