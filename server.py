283ff5f04912be19382cc0800a0e262abcb3b77a
marlenaKauer <mak4wd@virginia.edu>
2013-12-01 10:10:04 -0500

    Update server.py

--- server.py
+++ server.py
@@ -111,15 +111,7 @@ def upload_file():
 
 
     return 'Upload Successful'
-    #'''
-    #<!doctype html>
-    #<title>Upload new File</title>
-    #<h1>Upload new File</h1>
-    #<form action="" method=post enctype=multipart/form-data>
-    #<p><input type=file name=file>
-    #<input type=submit value=Upload>
-    #</form>
-    # '''
+  
 
 # view files uploaded to UPLOAD_FOLDER
 @fileApp.route('/onedir/<filename>')
